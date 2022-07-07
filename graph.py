2af7d1cb1659c4869203e42471c2a98b8b425c5a
thanhle4299 <108439175+thanhle4299@users.noreply.github.com>
2022-07-07 11:00:16 +0900

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,8 +1,10 @@
 ﻿import numpy as np
 import matplotlib.pyplot as plt
   
-x = np.linspace(-10, 10, 101)				
+x = np.linspace(-10, 10, 101)	
+print(x)
 y = np.exp(-x**2 / 2) / np.sqrt(2 * np.pi)
+print(y)
   
 plt.plot(x, y)
 plt.show()
